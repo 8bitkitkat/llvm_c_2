e850336e205d92b2bd80c5b0ae940bace8238946
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-07-09 22:17:00 +0200

    rnn.cxx: Add `use_mold` option to `runNinja`

--- .rnn/lib/cxx.ts
+++ .rnn/lib/cxx.ts
@@ -1,29 +1,34 @@
-import * as util from './util.ts';
+import * as util from "./util.ts";
 
 export type Version =
-  'default'
-  | '98'
-  | '11'
-  | '17'
-  | '20';
+  | "default"
+  | "98"
+  | "11"
+  | "17"
+  | "20";
 
 export type Library = {
-  name: string,
-  remote?: { git: string, recursive: boolean },
-  includes: string[],
-  linkDirs: string[],
-  links: string[],
-  defines: [string, string][],
-  setup?: () => Promise<void>,
+  name: string;
+  remote?: { git: string; recursive: boolean };
+  includes: string[];
+  linkDirs: string[];
+  links: string[];
+  defines: [string, string][];
+  setup?: () => Promise<void>;
 };
 
-export async function runNinja(what?: string, cwd?: string, logger?: util.LineLogger) {
-  logger?.log(`Running Ninja${what ? ` on ${what}` : ''}`);
+export async function runNinja(
+  what?: string,
+  cwd?: string,
+  logger?: util.LineLogger,
+  use_mold = false,
+) {
+  logger?.log(`Running Ninja${what ? ` on ${what}` : ""}`);
 
-  util.stdout.write('\n');
+  util.stdout.write("\n");
 
-  const cmd = `mold -run ninja${what ? ` ${what}` : ''}`;
-  const p = Deno.run({ cmd: cmd.split(' '), cwd });
+  const cmd = use_mold ? "mold -run " : "" + `ninja${what ? ` ${what}` : ""}`;
+  const p = Deno.run({ cmd: cmd.split(" "), cwd });
   const s = await p.status();
   if (!s.success) {
     // const decoder = new TextDecoder();
@@ -32,7 +37,7 @@ export async function runNinja(what?: string, cwd?: string, logger?: util.LineLo
     // throw new Error(`Failed executing '${cmd}'\n--- stdout:\n${stdout}\n--- stderr:\n${stderr}`);
     throw new Error(`Failed executing '${cmd}'`);
   }
-  p.close()
+  p.close();
 
   util.stdout.moveUp();
   util.stdout.moveUp();
