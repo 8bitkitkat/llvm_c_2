70e1e59dc741d4743185d74ab36fde62bcab33d7
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-07-17 21:15:49 +0200

    Add `llvm_Constant_getNullValue`

--- include/llvm_c_2/IR/Constant.h
+++ include/llvm_c_2/IR/Constant.h
@@ -11,6 +11,8 @@ LLVM_C_EXTERN_C_BEGIN
 /// Safe to cast to `LLVM_IR_ValueRef`
 typedef struct llvm_Constant_Opaque* llvm_ConstantRef;
 
-llvm_ConstantRef llvm_Constant_getIntegerValue(llvm_TypeRef, uint64_t);
+llvm_ConstantRef llvm_Constant_getIntegerValue(llvm_IntegerTypeRef, uint64_t);
+
+llvm_ConstantRef llvm_Constant_getNullValue(llvm_TypeRef);
 
 LLVM_C_EXTERN_C_END
--- src/IR/Constant.cpp
+++ src/IR/Constant.cpp
@@ -10,11 +10,15 @@
 
 LLVM_C_EXTERN_C_BEGIN
 
-llvm_ConstantRef llvm_Constant_getIntegerValue(llvm_TypeRef ref, uint64_t val) {
+llvm_ConstantRef llvm_Constant_getIntegerValue(llvm_IntegerTypeRef ref, uint64_t val) {
     auto type = reinterpret_cast<llvm::IntegerType*>(ref);
     auto i = llvm::APInt(type->getBitWidth(), val);
     return reinterpret_cast<llvm_ConstantRef>(llvm::Constant::getIntegerValue(type, i));
-    // llvm::GlobalVariable()
+}
+
+llvm_ConstantRef llvm_Constant_getNullValue(llvm_TypeRef ref) {
+    auto type = reinterpret_cast<llvm::Type*>(ref);
+    return reinterpret_cast<llvm_ConstantRef>(llvm::Constant::getNullValue(type));
 }
 
 LLVM_C_EXTERN_C_END
